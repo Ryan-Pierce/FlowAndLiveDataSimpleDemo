abfebf07076c850d06c7a6252f1a7a2e676158ed
RyanPierce <RyanPierce@gatech.edu>
2020-09-08 00:01:29 -0500

    Improved docs

--- app/src/main/java/me/ryanpierce/flowandlivedatasimpledemo/UserListAdapter.kt
+++ app/src/main/java/me/ryanpierce/flowandlivedatasimpledemo/UserListAdapter.kt
@@ -11,6 +11,20 @@ class UserListAdapter : ListAdapter<User, RecyclerView.ViewHolder>(UserDiffCallb
 
     private val list: MutableList<User> = mutableListOf()
 
+    /**
+     * To keep this demo simple, I recreated the `add()` functionality
+     * from ArrayAdapters in the pre-Jetpack era with `addItem()`. This was
+     * to demonstrate the mechanics of Flow by populating a RecyclerView
+     * with a Flow<data>.
+     * In modern Android development with the JetPack RecyclerView and
+     * ListAdapter, you might observe a Flow<List<data>> where the list
+     * of data is the original list, updated with the new data value. This
+     * ensures that the new state coming from your flow is the "full" new
+     * state and not just the part of your state that changed.
+     * You might call `submitList()` with the List<data> collected from
+     * each emission of the flow and `DiffUtil.ItemCallback` will ensure
+     * that existing values in the RecyclerView aren't duplicated.
+     */
     fun addItem(user: User) {
         list.add(user)
         submitList(list) { notifyDataSetChanged() }
